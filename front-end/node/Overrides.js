989531387c39d118d0c2195c4bb57e9341793adf
Danny Coates <dannycoates@gmail.com>
2012-05-24 23:54:20 -0700

    made debug-brk work

--- front-end/node/Overrides.js
+++ front-end/node/Overrides.js
@@ -1,63 +1,29 @@
-
 // Wire up websocket to talk to backend
 WebInspector.loaded = function() {
   WebInspector.socket = io.connect(
-    "http://" + window.location.host + '/',
+    "http://" + window.location.host + '/'
+    ,
     {
       reconnect: false
-    });
-  WebInspector.socket.on('message', function(message) {
-    if (message && message !== 'ping') {
-      InspectorBackend.dispatch(message);
     }
-  });
-  WebInspector.socket.on('error', function(error) { console.error(error); });
-  WebInspector.socket.on('connect', function() {
-      InspectorFrontendHost.sendMessageToBackend = WebInspector.socket.send.bind(WebInspector.socket);
-      WebInspector.doLoadedDone();
-  });
-  return;
-};
+  )
+  WebInspector.socket.on('message'
+    ,
+    function(message) {
+      if (message && message !== 'ping') {
+        InspectorBackend.dispatch(message)
+      }
+    }
+  )
+  WebInspector.socket.on('error', function(error) { console.error(error) })
+  WebInspector.socket.on('connect'
+    ,
+    function() {
+      InspectorFrontendHost.sendMessageToBackend = WebInspector.socket.send.bind(WebInspector.socket)
+      WebInspector.doLoadedDone()
+    }
+  )
+}
 
 // debugger always enabled
 Preferences.debuggerAlwaysEnabled = true;
-// enable LiveEdit
-// Preferences.canEditScriptSource = true;
-// // enable heap profiler
-// Preferences.heapProfilerPresent = true;
-
-// patch new watch expression (default crashes node)
-WebInspector.WatchExpressionsSection.NewWatchExpression = "''";
-
-// enable ctrl+click for conditional breakpoints
-// WebInspector.SourceFrame.prototype._mouseDown = function(event)
-// {
-//   this._resetHoverTimer();
-//   this._hidePopup();
-//   if (event.button != 0 || event.altKey || event.metaKey)
-//       return;
-//   var target = event.target.enclosingNodeOrSelfWithClass("webkit-line-number");
-//   if (!target)
-//       return;
-//   var row = target.parentElement;
-
-//   var lineNumber = row.lineNumber;
-
-//   var breakpoint = this._textModel.getAttribute(lineNumber, "breakpoint");
-//   if (breakpoint) {
-//       if (event.shiftKey) {
-//           breakpoint.enabled = !breakpoint.enabled;
-//       }
-//       else if (!event.ctrlKey) {
-//           breakpoint.remove();
-//       }
-//   } else {
-//       this._addBreakpointDelegate(lineNumber + 1);
-//       breakpoint = this._textModel.getAttribute(lineNumber, "breakpoint");
-//   }
-//   if (breakpoint && event.ctrlKey) {
-//       this._editBreakpointCondition(breakpoint);
-//   }
-//   event.preventDefault();
-// };
-
