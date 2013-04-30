9390aacad5a0e87f0a168668d017b24bc1277cbb
Miroslav Bajtoš <miro.bajtos@gmail.com>
2013-04-30 15:31:05 +0200

    Fixed breakpoint restore bug.
    Fixed bug where breakpoint restore did not work well
    with shortened script names.

--- test/ScriptManager.js
+++ test/ScriptManager.js
@@ -2,14 +2,14 @@ var expect = require('chai').expect,
     ScriptManager = require('../lib/ScriptManager').ScriptManager;
 
 describe('ScriptManager', function() {
+  var manager;
+
+  beforeEach(function() {
+    manager = new ScriptManager();
+  });
+
 
   describe('findSourceByID()', function() {
-    var manager;
-
-    beforeEach(function() {
-      manager = new ScriptManager();
-    });
-
     it('returns stored source', function() {
       manager._sources['id'] = 'a-source';
       expect(manager.findScriptByID('id')).to.equal('a-source');
@@ -19,4 +19,12 @@ describe('ScriptManager', function() {
       expect(manager.findScriptByID('unknown-id')).to.be.undefined;
     });
   });
+
+  describe('reset()', function() {
+    it('removes all stored scripts', function() {
+      manager._sources['id'] = 'a-source';
+      manager.reset();
+      expect(manager.findScriptByID('id')).to.be.undefined;
+    });
+  })
 });
