faad7b051fb4fc4cf64db2fec190e10ad0c24445
Miroslav Bajtos <miro.bajtos@gmail.com>
2013-05-20 07:51:25 +0200

    Removed test/hello.js
    Removed hello.js which was presumably used for manual testing. We have
    automated unit-tests in test folder now and hello.js did not play well
    with mocha framework.

--- test/hello.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-
-var x = 0;
-http.createServer(function (req, res) {
-  x += 1;
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World ' + x);
-}).listen(8124);
-console.log('Server running at http://127.0.0.1:8124/');
