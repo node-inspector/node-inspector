14d54b19eeb61e3f81c345376aec782b63dadab8
Miroslav Bajtos <miroslav@strongloop.com>
2013-09-12 11:06:30 +0100

    NetworkAgent: support data scheme URLs
    Implemented support for data scheme URLs. This allows sourcemap files to be
    embedded in the sourceMappingURL in the generated js file.

---
+++ test/NetworkAgent.js
@@ -0,0 +1,21 @@
+var expect = require('chai').expect,
+    NetworkAgent = require('../lib/NetworkAgent.js').NetworkAgent;
+
+describe('NetworkAgent', function() {
+  describe('loadResourceForFrontend', function() {
+    it('should load data URLs', function(done) {
+      var agent = new NetworkAgent();
+      agent.loadResourceForFrontend(
+        {
+          url: 'data:text/plain;base64,aGVsbG8gd29ybGQ='
+        },
+        function(err, result) {
+          if (err) return done(err);
+          expect(result.content).to.equal('hello world');
+          done();
+        }
+      );
+    });
+  });
+})
+
