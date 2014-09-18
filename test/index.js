d0265209af23f380bcc40e6731fbb3b4a9c1a2f7
mriehle <mriehle@arubanetworks.com>
2014-09-18 14:04:24 -0700

    Enable node-inspector listening on https.
    If both an ssl-key and ssl-cert are specified on the command line
    or in the config, the server will start up listening on https
    on the specified port.

---
+++ test/index.js
@@ -0,0 +1,30 @@
+var util = require('util'),
+    expect = require('chai').expect;
+
+var index = require('../index');
+
+describe('index', function() {
+  describe('build_url', function() {
+    it('should build an http URL', function() {
+      var url = index.buildInspectorUrl(
+        'example.com',
+        '2223',
+        '7863',
+        null,
+        false
+      );
+      expect(url).to.equal('http://example.com:2223/debug?port=7863');
+    });
+
+    it('should build an http URL', function() {
+      var url = index.buildInspectorUrl(
+        'example.com',
+        '2223',
+        '7863',
+        null,
+        true
+      );
+      expect(url).to.equal('https://example.com:2223/debug?port=7863');
+    });
+  });
+});
