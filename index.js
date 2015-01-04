9c7d3d7033ac74cd3474b08b41d59fde49cea4fe
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-01-04 16:46:54 +0100

    Added method to build front-end url, and refactored buildInspectorUrl-method to use buildFrontendUrl.

--- index.js
+++ index.js
@@ -2,21 +2,35 @@
 var url = require('url');
 
 exports.buildInspectorUrl = buildInspectorUrl;
+exports.buildFrontendUrl = buildFrontendUrl;
+
+/**
+ * Build a URL for loading frontend assets in the browser.
+ * @param {string} host
+ * @param {number} port
+ * @param {string} path
+ * @param {number} debugPort
+ * @param {boolean} toggle https
+ */
+function buildFrontendUrl(host, port, path, debugPort, isHttps) {
+  var parts = {
+    protocol: isHttps ? 'https' : 'http',
+    hostname: host,
+    port: port,
+    pathname: path,
+    search: debugPort ? '?port=' + debugPort : ''
+  };
+
+  return url.format(parts);
+}
 
 /**
  * Build a URL for loading inspector UI in the browser.
  * @param {string|undefined} inspectorHost as configured via --web-host
  * @param {number} inspectorPort as configured via --web-port
  * @param {number} debugPort as configured via --debug in the debugged app
+ * @param {boolean} is https or not
  */
 function buildInspectorUrl(inspectorHost, inspectorPort, debugPort, fileToShow, isHttps) {
-  var parts = {
-    protocol: isHttps ? 'https' : 'http',
-    hostname: inspectorHost == '0.0.0.0' ? '127.0.0.1' : inspectorHost,
-    port: inspectorPort,
-    pathname: '/debug',
-    search: '?port=' + debugPort
-  };
-
-  return url.format(parts);
+  return buildFrontendUrl(inspectorHost, inspectorPort, '/debug', debugPort, isHttps);
 }
