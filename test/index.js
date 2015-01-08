53171a0b9b8557e7e70d622dd6680dccde37c6fe
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-01-08 23:48:20 +0100

    Added a few unit tests for the new URL methods.

--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@ var util = require('util'),
 var index = require('../index');
 
 describe('index', function() {
-  describe('build_url', function() {
+  describe('buildInspectorUrl', function() {
 
     it('should build an http URL', function() {
       var url = index.buildInspectorUrl(
@@ -27,4 +27,57 @@ describe('index', function() {
     });
 
   });
+
+  describe('buildFrontendUrl', function() {
+
+    it('should build an http URL', function() {
+      var url = index.buildFrontendUrl(
+        'example.com',
+        '2223',
+        '/front-end-path',
+        '7863',
+        false
+      );
+      expect(url).to.equal('http://example.com:2223/front-end-path?port=7863');
+    });
+
+    it('should build an https URL', function() {
+      var url = index.buildFrontendUrl(
+        'example.com',
+        '2223',
+        '/front-end-path',
+        '7863',
+        true
+      );
+      expect(url).to.equal('https://example.com:2223/front-end-path?port=7863');
+    });
+
+  });
+
+  describe('buildWebSocketUrl', function() {
+
+    it('should build an ws URL', function() {
+      var url = index.buildWebSocketUrl(
+        'example.com',
+        '2223',
+        '7863',
+        false
+      );
+      expect(url).to.equal('ws://example.com:2223/ws?port=7863');
+    });
+
+    it('should build an wss URL', function() {
+      var url = index.buildWebSocketUrl(
+        'example.com',
+        '2223',
+        '7863',
+        true
+      );
+      expect(url).to.equal('wss://example.com:2223/ws?port=7863');
+    });
+
+  });
+
+
+
 });
