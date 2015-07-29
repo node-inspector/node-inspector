d758e03dd7c8296b84b56363a1fdc47c9f1f9e44
3y3 <3y3@bk.ru>
2015-07-29 10:17:38 +0300

    Update release.sh

--- tools/release.sh
+++ tools/release.sh
@@ -5,28 +5,49 @@ set -e
 VERSION=${1:?version is mandatory}
 TAG=v${VERSION}
 
+VERIFY=true
+PUBLISH_ONLY=false
+while [ "$1" != "" ]; do
+  case $1 in
+    -f | --forse | --no-verify )
+      VERIFY=false
+      ;;
+    -p | --publish )
+      PUBLISH_ONLY=true
+      ;;
+  esac
+  shift
+done
+
 echo --RELEASE $VERSION AS TAG $TAG--
 
-echo --Pull remote changes--
-git pull
 
-echo --Reinstall all dependencies--
-rm -rf node_modules && npm install
+if ! $PUBLISH_ONLY; then
 
-echo --Run the tests--
-npm test
+  if ! $VERIFY; then
+    echo --Pull remote changes--
+    git pull
 
-echo --Update the version in package.json--
-npm version --git-tag-version=false "$VERSION"
+    echo --Reinstall all dependencies--
+    rm -rf node_modules && npm install
 
-echo --Update ChangeLog.md--
-tools/git-changelog.sh -t "$VERSION"
+    echo --Run the tests--
+    npm test
+  fi
 
-echo --Commit the changes--
-git commit -m "$VERSION" ChangeLog.md package.json
 
-echo --Tag the release--
-tools/git-changelog.sh -l -t "$VERSION" | git tag -a "$TAG" -F-
+  echo --Update the version in package.json--
+  npm version --git-tag-version=false "$VERSION"
+
+  echo --Update ChangeLog.md--
+  tools/git-changelog.sh -t "$VERSION"
+
+  echo --Commit the changes--
+  git commit -m "$VERSION" ChangeLog.md package.json
+
+  echo --Tag the release--
+  tools/git-changelog.sh -l -t "$VERSION" | git tag -a "$TAG" -F-
+fi
 
 echo --Push to github--
 git push && git push origin "$TAG"
