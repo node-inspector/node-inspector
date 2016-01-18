530c059d459aff44c32458fbefec167138f118aa
Yury Puzynya <3y3@bk.ru>
2016-01-19 00:51:09 +0300

    Fix Profilers tests

--- test/ProfilerAgent.js
+++ test/ProfilerAgent.js
@@ -1,78 +1,50 @@
-var expect = require('chai').expect,
-    launcher = require('./helpers/launcher.js'),
-    EventEmitter = require('events').EventEmitter,
-    InjectorClient = require('../lib/InjectorClient').InjectorClient,
-    ProfilerAgent = require('../lib/Agents/ProfilerAgent').ProfilerAgent;
+'use strict';
 
-var profilerAgent,
-    debuggerClient,
-    frontendClient;
+var co = require('co');
+var expect = require('chai').expect;
+var launcher = require('./helpers/launcher.js');
+var InjectorClient = require('../lib/InjectorClient');
+var ProfilerAgent = require('../lib/Agents/ProfilerAgent');
 
-xdescribe('Profiler Agent', function() {
-  before(initializeProfiler);
+var session;
+var profilerAgent;
+var debuggerClient;
+var frontendClient;
 
-  it('should start profiling', function(done) {
-    profilerAgent.start({}, function(error, result){
-      if (error) return done(error);
+describe('Profiler Agent', function() {
+  before(() => initializeProfiler());
+
+  it('should start profiling', () => {
+    return co(function * () {
+      var result = yield profilerAgent.handle('Profile.start');
       expect(result).to.equal(undefined);
-      done();
     });
   });
 
-  it('should stop profiling', function(done) {
-    profilerAgent.stop({}, function(error, result) {
-      if (error) return done(error);
+  it('should stop profiling', () => {
+    return co(function * () {
+      var result = yield profilerAgent.handle('Profile.stop');
       expect(result).to.have.property('profile');
-      done();
-    });
-  });
-
-  describe('nodeVersionIsCompatible', function() {
-    it('returns false for v0.10.29', function(done) {
-      expect(ProfilerAgent.nodeVersionIsCompatible('v0.10.29'))
-        .to.equal(false);
-      done();
-    });
-
-    it('returns false for v0.11.12', function(done) {
-      expect(ProfilerAgent.nodeVersionIsCompatible('v0.11.12'))
-        .to.equal(false);
-      done();
-    });
-
-    it('returns true for v0.11.13', function(done) {
-      expect(ProfilerAgent.nodeVersionIsCompatible('v0.11.13'))
-        .to.equal(true);
-      done();
-    });
-
-    it('returns true for v0.12.0', function(done) {
-      expect(ProfilerAgent.nodeVersionIsCompatible('v0.12.0'))
-        .to.equal(true);
-      done();
-    });
-
-    it('returns true for v1.0.0', function(done) {
-      expect(ProfilerAgent.nodeVersionIsCompatible('v1.0.0'))
-        .to.equal(true);
-      done();
     });
   });
 });
 
-function initializeProfiler(done) {
-  launcher.runCommandlet(true, function(childProcess, session) {
-    debuggerClient = session.debuggerClient;
-    frontendClient = session.frontendClient;
+function expand(instance) {
+  session = instance.session;
+  debuggerClient = session.debuggerClient;
+  frontendClient = session.frontendClient;
+}
+
+function initializeProfiler() {
+  return co(function * () {
+    yield launcher.runCommandlet(true).then(expand);
 
     var injectorClient = new InjectorClient({}, session);
     session.injectorClient = injectorClient;
 
     profilerAgent = new ProfilerAgent({}, session);
 
-    injectorClient.inject(function(error) {
-      if (error) return done(error);
-      debuggerClient.request('continue', null, done);
-    });
+    yield injectorClient.injected();
+    yield debuggerClient.request('continue');
   });
 }
