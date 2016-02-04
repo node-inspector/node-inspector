c24f2ce6d5fd077eec4785530148e9aeea309803
Jason <jason.daurus@gmail.com>
2016-02-04 22:10:18 +0800

    [node-debug] Gracefully exit after receiving SIGINT

--- bin/node-debug.js
+++ bin/node-debug.js
@@ -45,6 +45,10 @@ function main() {
     process.exit();
   }
 
+  process.on('SIGINT', () => {
+    process.exit();
+  });
+
   startInspector(function(err) {
     if (err) {
       console.error(formatNodeInspectorError(err));
@@ -197,13 +201,13 @@ function openBrowserAndPrintInfo() {
     // try to launch the URL in one of those browsers in the defined order
     // (but if one of them is default browser, then it takes priority)
     open(url, {
-        preferredBrowsers : ['chrome', 'chromium', 'opera']
-      }, function(err, okMsg) {
-        if (err) {
-           // unable to launch one of preferred browsers for some reason
-           console.log(err.message);
-           console.log('Please open the URL manually in Chrome/Chromium/Opera or similar browser');
-        }
+      preferredBrowsers : ['chrome', 'chromium', 'opera']
+    }, function(err, okMsg) {
+      if (err) {
+         // unable to launch one of preferred browsers for some reason
+         console.log(err.message);
+         console.log('Please open the URL manually in Chrome/Chromium/Opera or similar browser');
+      }
     });
   }
 
